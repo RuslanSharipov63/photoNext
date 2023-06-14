dff9d1b567d319a3e669527ad6b1d89dee4bdfd6
RuslanSharipov63 <sharipov.r@mail.ru>
2023-06-14 15:56:57 +0400

    add validator and schema

--- back/server.js
+++ back/server.js
@@ -1,7 +1,10 @@
-const express = require ('express');
+const express = require('express');
+const { registerValidator } = require('./validations/auth')
 const PORT = 4000;
-const app = express(); 
+const app = express();
+
+
 
 app.listen(PORT, (err) => {
-err ? console.log(err) : console.log(`Listening port ${PORT}`)
+    err ? console.log(err) : console.log(`Listening port ${PORT}`)
 })
