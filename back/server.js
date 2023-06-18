7d211528a0250f60e8ab1f3335eb19236490eced
unknown <sharipov.r@mail.ru>
2023-06-19 00:25:27 +0400

    create logic token

--- back/server.js
+++ back/server.js
@@ -1,53 +1,115 @@
-const express = require('express');
-const registerValidation = require('./validations/auth')
-const { validationResult } = require('express-validator');
-const UserModel = require('./models/User');
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
-/* const { connectionToDb, getDb } = require('./db') */
+const express = require("express");
+const registerValidation = require("./validations/auth");
+const { validationResult } = require("express-validator");
+const UserModel = require("./models/User");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const checkAuth = require("./utils/checkAuth");
 
 const PORT = 4000;
+const URL = "mongodb://localhost:27017/photobox";
+
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 mongoose
-    .connect('mongodb://localhost:27017/photobox')
-    .then(() => console.log('DB ok'))
-    .catch((err) => console.log('DB error', err))
+  .connect(URL)
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
+/* авторизация */
+app.post("/auth/login", async (req, res) => {
+  try {
+    /* сначала мы должны понять есть ли пользователь в базе данных */
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      /* мы уточняем почему не происходит авторизация только для себя, в реальном приложении ни в коем случае нельзя говорить, что именно не совпадает, нужно максимально обще сообщать, например, неверный пользователь или пароль */
+      return res.status(404).json({ message: "Пользователь не найден" });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPass) {
+      return res.status(400).json({ message: "Неверный логин или пароль" });
+    }
+    /* если пользователь авторизовался, то мы создаем новый токен */
+    const token = jwt.sign({ _id: user._id }, "secret123", {
+      expiresIn: "30d",
+    });
 
+    const { passwordHash, ...userData } = user._doc;
 
-app.post('/auth/register', registerValidation, async (req, res) => {
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось авторизоваться",
+    });
+  }
+});
+
+/* регистрация */
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+      return res.status(400).json(errors.array());
     }
 
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt)
+    const hash = await bcrypt.hash(password, salt);
 
     const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarURL: req.body.avatarURL,
-        passwordHash,
+      fullName: req.body.fullName,
+      email: req.body.email,
+      avatarURL: req.body.avatarURL,
+      passwordHash: hash,
+    });
 
-    })
+    const user = await doc.save();
+    const token = jwt.sign({ _id: user._id }, "secret123", {
+      expiresIn: "30d",
+    });
 
-    const user = doc.save();
+    const { passwordHash, ...userData } = user._doc;
 
-    res.json(user)
-})
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
+  }
+});
 
+/* нам нужно проверить можем ли мы получить информацию о себе */
+/* может ли пользователь получить информацию о себе */
 
-/* let db;
-connectionToDb((error) => {
-    if (!error) {
-        app.listen(PORT, (err) => {
-            err ? console.log(err) : console.log(`Listening port ${PORT}`)
-        })
-        db = getDb();
-    } else {
-        console.log(`DB connection error: ${error}`)
+app.get("auth/me", checkAuth, async (req, res) => {
+  try {
+    /* тут мы должны расшифровать токен, который будем передавать и решить может ли пользователь получить информацию о себе. для того надо сделать специальную функцию - checkAuth - она передается вторым параметром*/
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
     }
-}) */
+    res.json({
+      succes: true,
+    });
+  } catch (error) {}
+});
--- back/validations/auth.js
+++ back/validations/auth.js
@@ -1,7 +1,8 @@
 const { body } = require('express-validator')
-module.exports = [
-    body('email', 'Неверный формат электронной почта').isEmail(),
-    body('password', 'Пароль слишком короткий. Минимум 5 символов').isLength({ min: 5 }),
+const registerValidation = [
     body('fullName', 'Укажите имя').isLength({ min: 3 }),
-    body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
+    body('email', 'Неверный формат электронной почты').isEmail(),
+    body('password', 'Пароль слишком короткий. Минимум 5 символов').isLength({ min: 5 }),
+    body('avatarURL', 'Неверная ссылка на аватарку').optional().isURL(),
 ]
+module.exports = registerValidation;
